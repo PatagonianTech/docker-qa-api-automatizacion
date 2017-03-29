d7ddfa4b5b01fc5f4a347058d697ca42f8a4b6e5
Mariano Sckerl <mariano.sckerl@patagonian.it>
2017-03-29 09:53:14 -0300

    nuevo build - agrega alias al bash

--- build.sh
+++ build.sh
@@ -9,7 +9,27 @@
 
 
 if [ -z "$TEST_MOCHA_NAME" ]; then
-	export TEST_MOCHA_NAME=testAPI-generic
+	export TEST_MOCHA_NAME=testapi-generic
 fi
 
-docker build --no-cache -t $TEST_MOCHA_NAME .
+#docker build --no-cache -t $TEST_MOCHA_NAME .
+
+TITLE="Define Env Variable for Docker image and add a global run command"
+
+installAlias() {
+  local f="$1"
+  if [ -f "$f" ]; then
+    if ! grep -q "${TITLE}" "$f"
+      then  
+        echo '' >> "$f"
+        echo "# ${TITLE}" >> "$f"
+        echo "export TEST_MOCHA_NAME='$TEST_MOCHA_NAME'" >> "$f"
+        echo "alias 'run-test-api'='docker run -it --rm --net=host -v \"\$(pwd):/app/context\" -e HOST_USER=\$UID -e HOST_GROUP=\$(id -g \$USERNAME) -e REPDIR=\"./reporte\" -e REPNAME=\"ApiRest\" -e REPTITLE=\"Titulo\" \$TEST_MOCHA_NAME -- \$@'" >> "$f"
+        echo '' >> "$f"
+      fi
+  fi
+}
+
+installAlias ~/.bashrc
+installAlias ~/.zshrc
+installAlias ~/.shrc
